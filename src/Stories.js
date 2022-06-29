0e22ea557794a0e4aef20ef18b49290863d8ab4a
aj-1904 <jainayush827@gmail.com>
2022-06-29 11:24:03 +0530

    API Calling and Data Fetching

--- src/Stories.js
+++ src/Stories.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function Stories() {
-  return <div>Stories</div>;
+  let isLoading = true;
+
+  let API = "http://hn.algolia.com/api/v1/search?query=html";
+
+  const fetchApiData = async (url) => {
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+      //   isLoading = false
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchApiData(API);
+  }, []);
+
+  if (isLoading) {
+    return <h1>Loading.....</h1>;
+  }
+
+  return (
+    <>
+      <h2>My Tech News Post</h2>
+    </>
+  );
 }
 
 export default Stories;
